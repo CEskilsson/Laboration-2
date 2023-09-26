9af1ae4c337b1ebf4a8db28da045e14fe090abf0
Cornelia Eskilsson <ce223ex@student.lnu.se>
2023-09-26 12:56:46 +0200

    Add createWordCloud in index.js

--- src/index.js
+++ src/index.js
@@ -14,40 +14,40 @@ const averageWordLengthButton = document.getElementById('average-word-length-but
 const averageWordLengthDisplay = document.getElementById('average-word-lenght-display')
 const wordFrequencyButton = document.getElementById('word-frequency-button');
 const wordFrequencyDisplay = document.getElementById('word-frequency')
+const generateWordCloudButton = document.getElementById('generate-word-cloud-button')
+const generateWordCloudDispaly = document.getElementById('word-cloud-container')
 
 wordCountButton.addEventListener('click', () => {
-  const textInput = document.getElementById('text-input').value
+  const textInput = document.getElementById('text-input-element').value
   const wordCount = countWords(textInput)
   wordCountDisplay.innerHTML = wordCount
 })
 
 periodCountButton.addEventListener('click', () => {
-  const textInput = document.getElementById('text-input').value
+  const textInput = document.getElementById('text-input-element').value
   const periodCount = countPeriods(textInput)
   periodCountDisplay.innerHTML = periodCount
-  
+
 })
 
 averageWordLengthButton.addEventListener('click', () => {
-  const textInput = document.getElementById('text-input').value
+  const textInput = document.getElementById('text-input-element').value
   const averageLength = calculateAverageWordLength(textInput)
   averageWordLengthDisplay.innerHTML = averageLength
-  
+
 })
 
 wordFrequencyButton.addEventListener('click', () => {
-  const textInput = document.getElementById('text-input').value
+  const textInput = document.getElementById('text-input-element').value
   const wordMap = wordFrequency(textInput)
 
-  // Clear previous content
-  wordFrequencyDisplay.innerHTML = ''
+  sortWordFrequency(wordMap)
+})
 
-  // Display word frequency results
-  for (const word in wordMap) {
-    const wordElement = document.createElement('p')
-    wordElement.textContent = `${word}: ${wordMap[word]}`
-    wordFrequencyDisplay.appendChild(wordElement)
-  }
+generateWordCloudButton.addEventListener('click', () => {
+  const textInput = document.getElementById('text-input-element').value
+  generateWordCloudDispaly.innerHTML = ''
+  createWordCloud(textInput)
 })
 
 function countWords(text) {
@@ -55,7 +55,7 @@ function countWords(text) {
   return words.length
 }
 
-function countPeriods (text) {
+function countPeriods(text) {
   return (text.match(/\./g) || []).length
 }
 
@@ -78,4 +78,47 @@ function wordFrequency(text) {
     }
   }
   return wordMap
+}
+
+function sortWordFrequency(wordMap) {
+  // Clear previous content
+  wordFrequencyDisplay.innerHTML = ''
+
+  let row = ''
+  let count = 0
+
+  for (const word in wordMap) {
+    if (count >= 5) {
+      wordFrequencyDisplay.innerHTML += `<p>${row}</p>`
+      row = ''
+      count = 0
+    }
+
+    row += `${word}: ${wordMap[word]}, `
+    count++
+  }
+  if (row !== '') {
+    wordFrequencyDisplay.innerHTML += `<p>${row}</p>`
+  }
+}
+
+function createWordCloud(text) {
+  const wordMap = wordFrequency(text)
+
+  const cloudContainer = document.getElementById('word-cloud-container')
+  cloudContainer.innerHTML = '' // Clear previous content
+
+  for (const word in wordMap) {
+    const wordElement = document.createElement('span')
+    wordElement.textContent = word
+    wordElement.className = `word freq-${wordMap[word]}`
+
+    const left = Math.random() * (cloudContainer.clientWidth - 100)
+    const top = Math.random() * (cloudContainer.clientHeight - 20)
+    wordElement.style.left = `${left}px`
+    wordElement.style.top = `${top}px`
+
+    cloudContainer.appendChild(wordElement)
+  }
+
 }
