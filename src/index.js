11412017ae7ace0b1320bb45e8e153e48fe36825
Cornelia Eskilsson <ce223ex@student.lnu.se>
2023-11-14 14:58:34 +0100

    Add word counter to input field

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { entries, addNewEntry } from './journal/journal-entries.js'
+import { TextAnalysis } from './modules/text-analysis-functions.js'
+
 /**
  * Entry point for the application.
  *
@@ -10,6 +12,25 @@ document.addEventListener('DOMContentLoaded', () => {
   const entryForm = document.getElementById('add-entry-form')
   const entryTextArea = document.getElementById('entry-text')
   const journalEntriesSection = document.getElementById('journal-entries')
+  const wordCountDisplay = document.getElementById('word-count')
+
+  const textAnalyzer = new TextAnalysis()
+
+  /**
+   * Updates the word count display based on the given text.
+   *
+   * @param {string} text - The text to be analyzed.
+   */
+  const updateWordCount = (text) => {
+    const wordCount = textAnalyzer.countWords(text)
+    wordCountDisplay.textContent = `Word count: ${wordCount}`
+  }
+
+  // Event listener for input changes
+  entryTextArea.addEventListener('input', (event) => {
+    const newText = event.target.value
+    updateWordCount(newText)
+  })
 
   /**
    * Displays the entries in the journal.
@@ -34,4 +55,17 @@ document.addEventListener('DOMContentLoaded', () => {
       entryTextArea.value = ''
     }
   })
+
+  /**
+   * Displays the word count in the text area.
+   */
+  const displayWordCount = () => {
+    const entryText = entryTextArea.value
+    const wordCount = textAnalyzer.countWords(entryText)
+    const wordCountDisplay = document.getElementById('word-count-display')
+    wordCountDisplay.textContent = `Word count: ${wordCount}`
+  }
+
+  // Event listener for textarea input
+  entryTextArea.addEventListener('input', displayWordCount)
 })
